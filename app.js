434f44e955e4803791d00581259bb9e44c3428f1
Luca Eliseo <121867356+LucaEliseo@users.noreply.github.com>
2024-02-18 03:08:39 +0100

    App.js and Main Style

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const btn = document.getElementById('btnGenerate');
 const inputText = document.getElementById('input');
 const imageSection = document.getElementById('imageSection');
 
+
 const getImages = async () => {
     const option = {
         method: 'POST',
@@ -22,7 +23,7 @@ const getImages = async () => {
             height: 1024,
             width: 1024,
             steps: 30,
-            samples: 1,
+            samples: 2,
         }),
     };
 
@@ -32,18 +33,31 @@ const getImages = async () => {
         console.log(data);
 
         if (data && data.artifacts) {
-    
-            while (imageSection.firstChild) {
-                imageSection.removeChild(imageSection.firstChild);
-            }
+            const imageContainer = document.getElementById('imageContainer');
+            imageContainer.innerHTML = ''; // Cancella il contenuto precedente
 
             data.artifacts.forEach(imageObject => {
-                const imageContainer = document.createElement('div');
-                imageContainer.classList.add('image-container');
+                const imageColumn = document.createElement('div');
+                imageColumn.classList.add('col');
+
                 const imageElement = document.createElement('img');
+                imageElement.classList.add('img-fluid');
                 imageElement.setAttribute('src', `data:image/png;base64,${imageObject.base64}`);
-                imageContainer.append(imageElement);
-                imageSection.append(imageContainer);
+
+                const imageCard = document.createElement('div');
+                imageCard.classList.add('card');
+                imageCard.appendChild(imageElement);
+
+                const imageCardBody = document.createElement('div');
+                imageCardBody.classList.add('card-body');
+                const imageCardText = document.createElement('p');
+                imageCardText.classList.add('card-text');
+                imageCardText.textContent = imageObject.text_prompt;
+                imageCardBody.appendChild(imageCardText);
+                imageCard.appendChild(imageCardBody);
+
+                imageColumn.appendChild(imageCard);
+                imageContainer.appendChild(imageColumn);
             });
         } else {
             console.error("La risposta dell'API non contiene dati validi.");
